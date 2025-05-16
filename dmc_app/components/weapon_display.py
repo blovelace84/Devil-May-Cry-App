2eff71da4d28192ca7c7a0b9c5843fcc6de6d758
blovelace84 <blovelace84@gmail.com>
2025-05-16 06:18:10 -0400

    adding images to weapons showcase

--- dmc_app/components/weapon_display.py
+++ dmc_app/components/weapon_display.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from PIL import Image
 
 def display_weapon_info(weapon):
     st.markdown(f"<h3 class='weapon-name'>{weapon['name']}</h3>", unsafe_allow_html=True)
+    image_path = weapon["image"]  # Get the image path
+    try:
+        image = Image.open(image_path)
+        st.image(image, width=300, use_container_width=False, output_format="JPEG")
+    except FileNotFoundError:
+        st.warning(f"Image not found: {image_path}")
     st.write(weapon["description"])
     st.write(f"**Type:** {weapon['type']}")
     st.write(f"**Damage:** {weapon['damage']}")
--- dmc_app/data/weapon_data.py
+++ dmc_app/data/weapon_data.py
@@ -1,24 +1,28 @@
 weapons = {
         "Rebellion": {
             "name": "Rebellion",
+            "image": "../weapons_images/rebellion.jpeg",
             "description": "Dante's signature sword, inherited from his father Sparda.  It can transform into different forms.",
             "type": "Sword",
             "damage": "High",
         },
         "Ebony & Ivory": {
             "name": "Ebony & Ivory",
+            "image": "../weapons_images/ebony_ivory.jpeg",
             "description": "Dante's custom-made handguns, effective at long range and for rapid firing.",
             "type": "Handguns",
             "damage": "Medium",
         },
         "Yamato": {
             "name": "Yamato",
+            "image": "../weapons_images/yamato.jpeg",
             "description": "Vergil's katana, a powerful weapon that can slice through dimensions.",
             "type": "Katana",
             "damage": "Very High",
         },
         "Devil Bringer": {
             "name": "Devil Bringer",
+            "image": "../weapons_images/devil_bringer.jpeg",
             "description": "Nero's mysterious right arm, which can perform powerful demonic attacks and grab enemies.",
             "type": "Arm",
             "damage": "Variable",
